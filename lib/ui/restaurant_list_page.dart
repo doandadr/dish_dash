1f604896fc41cd83db3692f8ec91697cf380ab78
Doanda Dresta Rahma <ddoanda@gmail.com>
2024-01-15 20:16:33 +0700

    feat: connect provider with restaurant list

--- lib/ui/restaurant_list_page.dart
+++ lib/ui/restaurant_list_page.dart
@@ -1,14 +1,13 @@
-import 'dart:convert';
-
 import 'package:dish_dash/common/style.dart';
 import 'package:dish_dash/data/api/api_service.dart';
-import 'package:dish_dash/data/model/restaurant.dart';
 import 'package:dish_dash/provider/restaurant_list_provider.dart';
 import 'package:dish_dash/widget/card_restaurant.dart';
 import 'package:dish_dash/widget/platform_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+import '../provider/result_state.dart';
 
 class RestaurantListPage extends StatelessWidget {
   static const String restaurantListTitle = 'Restaurants';
@@ -18,32 +17,51 @@ class RestaurantListPage extends StatelessWidget {
   });
 
   Widget _buildList(BuildContext context) {
-    return FutureBuilder(
-      future: DefaultAssetBundle.of(context)
-          .loadString('assets/local_restaurant.json'),
-      builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-        final List<Restaurant> restaurants = _parseRestaurants(snapshot.data);
-        return ListView.separated(
-          itemCount: restaurants.length,
-          itemBuilder: (context, index) {
-            var restaurant = restaurants[index];
-            return CardRestaurant(restaurant: restaurant);
-          },
-          separatorBuilder: (BuildContext context, int index) {
-            return const Divider(
-              height: 1,
-            );
-          },
+    return Consumer<RestaurantListProvider>(builder: (context, provider, _) {
+      if (provider.state == ResultState.loading) {
+        return const Center(
+          child: CircularProgressIndicator(),
         );
-      },
-    );
+      } else {
+        if (provider.state == ResultState.hasData) {
+          return ListView.separated(
+            itemCount: provider.result.restaurants.length,
+            itemBuilder: (context, index) {
+              var restaurant = provider.result.restaurants[index];
+              return CardRestaurant(restaurant: restaurant);
+            },
+            separatorBuilder: (BuildContext context, int index) {
+              return const Divider(
+                height: 1,
+              );
+            },
+          );
+        } else if (provider.state == ResultState.noData) {
+          return Center(
+            child: Material(
+              child: Text(provider.message),
+            ),
+          );
+        } else if (provider.state == ResultState.error) {
+          return const Center (
+            child: Material(
+              child: Text("Error: Could not receive data from network"),
+            ),
+          );
+        }else {
+          return const Material(
+            child: Text(''),
+          );
+        }
+      }
+    });
   }
 
   Widget _buildAndroid(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'Restaurants',
+          'Browse Restaurants',
           style: TextStyle(color: primaryColor, fontFamily: 'Hero'),
         ),
         elevation: 6,
@@ -68,9 +86,9 @@ class RestaurantListPage extends StatelessWidget {
           color: primaryColor,
         ),
         middle: Text(
-        'Restaurants',
-        style: TextStyle(color: primaryColor, fontFamily: 'Hero'),
-      ),
+          'Browse Restaurants',
+          style: TextStyle(color: primaryColor, fontFamily: 'Hero'),
+        ),
         transitionBetweenRoutes: false,
       ),
       child: _buildList(context),
@@ -83,15 +101,8 @@ class RestaurantListPage extends StatelessWidget {
       create: (_) {
         return RestaurantListProvider(apiService: ApiService());
       },
-      child: PlatformWidget(
-          androidBuilder: _buildAndroid, iosBuilder: _buildIos),
+      child:
+          PlatformWidget(androidBuilder: _buildAndroid, iosBuilder: _buildIos),
     );
-  }
-
-  List<Restaurant> _parseRestaurants(String? json) {
-    if (json == null) return [];
-
-    final List parsed = jsonDecode(json);
-    return parsed.map((json) => Restaurant.fromJson(json)).toList();
   }
 }
